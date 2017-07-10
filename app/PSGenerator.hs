ccd0045b3dc3745a7c22722120189b4a2ca87bd9
kejace <kejace@gmail.com>
2017-07-10 18:56:09 -0400

    Updated example

--- app/PSGenerator.hs
+++ app/PSGenerator.hs
@@ -11,15 +11,8 @@ import           Servant.Subscriber.Response
 import           Servant.Subscriber.Types
 
 import           Control.Applicative
-import           Control.Lens
-import           Data.Aeson
 import           Data.Monoid
 import           Data.Proxy
-import qualified Data.Set                           as Set
-import           Data.Text                          (Text)
-import qualified Data.Text                          as T
-import qualified Data.Text.Encoding                 as T
-import qualified Data.Text.IO                       as T
 import           Language.PureScript.Bridge
 import           Language.PureScript.Bridge.PSTypes
 import           Servant.API
