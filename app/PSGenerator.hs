86e5af287021d5e7cd67029bdec2bcd2fa4f41c1
kejace <kejace@gmail.com>
2017-07-10 18:16:27 -0400

    Updated example

--- app/PSGenerator.hs
+++ app/PSGenerator.hs
@@ -1,12 +1,30 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE AutoDeriveTypeable    #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
 
-import           Data.Proxy                         (Proxy (..))
-import           Language.PureScript.Bridge
-import           Language.PureScript.Bridge.PSTypes
 import           Servant.Subscriber.Request
 import           Servant.Subscriber.Response
 import           Servant.Subscriber.Types
 
+import           Control.Applicative
+import           Control.Lens
+import           Data.Aeson
+import           Data.Monoid
+import           Data.Proxy
+import qualified Data.Set                           as Set
+import           Data.Text                          (Text)
+import qualified Data.Text                          as T
+import qualified Data.Text.Encoding                 as T
+import qualified Data.Text.IO                       as T
+import           Language.PureScript.Bridge
+import           Language.PureScript.Bridge.PSTypes
+import           Servant.API
+import           Servant.PureScript
+import           Servant.Subscriber.Subscribable
 
 myTypes = [
        mkSumType (Proxy :: Proxy Request)
